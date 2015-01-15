c34a68d781a81d1fc607719643f96ab3c16587fb
nayanaith <nayanaith@gmail.com>
2015-01-15 16:02:34 -0500

    again

---
+++ src/main/java/framework/one/LoginPage.java
@@ -0,0 +1,9 @@
+package framework.one;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage implements TestDataConstants{
+	WebDriver driver;
+	By gmlUserName="nayanaith.tablet";
+	By gmlPwd="";
+}//end BasePage
